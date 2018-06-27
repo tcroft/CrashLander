f43b1411039692b4295ddab652d8b96c645fa262
Mayur Gawade <mgawade@PUNmgawadeNQG3QD.local>
2018-06-27 18:59:46 +0530

    Added Crashed Down And Successful Landing Animation With Message

--- game.js
+++ game.js
@@ -7,29 +7,49 @@ var shipPosition;
 var shipSpeed;
 var gravity = .00001;
 var thrustPower = .00002;
+var fuel=100;
+//var speed;
 var thrust = false;
+var meter;
+var done;
 
 function initGame() {
     lander = new Lander('ship');
     lander.scaleTo(.5);
-
     canvas = document.getElementById('canvas');
-
+    meter=document.getElementById("fuel");
+    //speed=document.getElementById("shipSpeed");
     window.setInterval(loop, 10);
-
+    // document.getElementById("crashed").innerHTML="";
     reset();
 }
 
 function loop() {
-
     if (thrust) {
-        shipSpeed -= thrustPower;
+        if((fuel>0)){
+            fuel-=0.25;
+            display(fuel);
+            shipSpeed -= thrustPower; 
+        } 
+        
     }
 
-    if (shipPosition > 0 || thrust) {
+    if (shipPosition > 0 || (thrust&&fuel>0)) {
         shipSpeed += gravity;
         shipPosition -= shipSpeed;
+        
     } else {
+        if(!done){
+        if(shipSpeed<0.0012){
+            displaysuccessmessage();
+        }
+        else{
+            displaycrashedmessage();
+            done=true;
+        }
+        
+    }
+
         shipSpeed = 0;
         shipPosition = 0;
     }
@@ -39,7 +59,8 @@ function loop() {
 function reset() {
     shipPosition = 1;
     shipSpeed = 0;
-
+    meter.value=100;
+    // ship.style.background="url('images/ship.png')";
     layout();
 }
 
@@ -56,4 +77,19 @@ function layout() {
     var distance = height - (height * shipPosition);
 
     lander.moveTo(canvas.clientWidth/2, lander.height()/2 + distance);
+}
+
+function display(fuel){
+    meter.value=fuel;
+    meter.speed=shipSpeed;
+}
+
+function displaysuccessmessage(){
+    document.getElementById("landed").innerHTML="!!! Landed Successfully !!!";
+    ship.style.background="url('images/successlanded.gif')";
+}
+
+function displaycrashedmessage(){
+    document.getElementById("crashed").innerHTML="!!! Game Over !!!";
+    ship.style.background="url('images/explosion.jpg')";
 }
