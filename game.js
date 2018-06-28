ba86f4b59a2cb8384ce28269f6d704325b6aa218
Mayur Gawade <mgawade@PUNmgawadeNQG3QD.local>
2018-06-28 14:22:15 +0530

    buttons Added

--- game.js
+++ game.js
@@ -1,7 +1,7 @@
 
 var lander;
 var canvas;
-
+//var flaming;
 var state;
 var shipPosition;
 var shipSpeed;
@@ -13,12 +13,30 @@ var thrust = false;
 var meter;
 var done;
 
+function initFlaming()
+{
+    flaming = new flaming('flaming');
+    flaming.scaleTo(.2);   
+}
+
+function showFlaming()
+{
+    var flaming = document.getElementById('flaming');
+    flaming.style.display = 'block';
+}
+function hideFlaming()
+{
+var flaming = document.getElementById('flaming');
+    flaming.style.display = 'none';
+}
+
 function initGame() {
     lander = new Lander('ship');
     lander.scaleTo(.5);
     canvas = document.getElementById('canvas');
     meter=document.getElementById("fuel");
     window.setInterval(loop, 10);
+    initFlaming();
     reset();
 }
 
@@ -58,16 +76,22 @@ function reset() {
     shipPosition = 1;
     shipSpeed = 0;
     meter.value=100;
+    done=false;
+    fuel=100;
     ship.style.background="url('images/ship.png')";
+    document.getElementById("landed").innerHTML=" ";
+    document.getElementById("crashed").innerHTML=" ";
     layout();
 }
 
 function startThrust() {
     thrust = true;
+     showFlaming();
 }
 
 function stopThrust() {
     thrust = false;
+    hideFlaming();
 }
 
 function layout() {
@@ -85,9 +109,17 @@ function display(fuel){
 function displaysuccessmessage(){
     document.getElementById("landed").innerHTML="!!! Landed Successfully !!!";
     ship.style.background="url('images/successlanded.gif') no-repeat";
+    document.getElementById("tryagain").innerHTML="Try Again";
 }
 
 function displaycrashedmessage(){
     document.getElementById("crashed").innerHTML="!!! Game Over !!!";
-    ship.style.background="url('images/explosion.jpg') no-repeat";
+    ship.style.background="url('images/explosion.png') no-repeat";
+    document.getElementById("tryagain").innerHTML="Try Again";
+ 
 }
+
+function refreshPage(){
+    window.location.reload();
+} 
+
